7fd017dbb9a00e32802c696eb3503e58eea3b7af
yukonisen#QwQ <whenten99@gmail.com>
2026-02-23 21:58:32 +0800

    更新统计

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -5,12 +5,24 @@ import "@varlet/ui/es/style";
 import "./components/plugin-repository/style/main.css";
 import PluginRepositoryApp from "./components/plugin-repository/App.vue";
 import DownloadPage from "./components/download/DownloadPage.vue";
+import VueMatomo from "vue-matomo";
 
 export default defineClientConfig({
-  enhance({ app }) {
+  enhance({ app, router }) {
     app.use(createPinia());
     app.use(Varlet);
     app.component("PluginRepositoryApp", PluginRepositoryApp);
     app.component("DownloadPage", DownloadPage);
+    
+    app.use(VueMatomo, {
+      host: "https://analytics.curiousers.org/",
+      siteId: 3,
+      router: router,
+      enableLinkTracking: true,
+      trackInitialView: true,
+      disableCookies: false,
+      requireConsent: false,
+      enableHeartBeatTimer: false,
+    });
   },
 });
