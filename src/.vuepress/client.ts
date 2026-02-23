0134f273d9800585af5248316cdeb500aeafc157
yukonisen#QwQ <whenten99@gmail.com>
2026-02-23 22:03:30 +0800

    修复构建

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -5,7 +5,6 @@ import "@varlet/ui/es/style";
 import "./components/plugin-repository/style/main.css";
 import PluginRepositoryApp from "./components/plugin-repository/App.vue";
 import DownloadPage from "./components/download/DownloadPage.vue";
-import VueMatomo from "vue-matomo";
 
 export default defineClientConfig({
   enhance({ app, router }) {
@@ -14,15 +13,20 @@ export default defineClientConfig({
     app.component("PluginRepositoryApp", PluginRepositoryApp);
     app.component("DownloadPage", DownloadPage);
     
-    app.use(VueMatomo, {
-      host: "https://analytics.curiousers.org/",
-      siteId: 3,
-      router: router,
-      enableLinkTracking: true,
-      trackInitialView: true,
-      disableCookies: false,
-      requireConsent: false,
-      enableHeartBeatTimer: false,
-    });
+    if (typeof window !== "undefined") {
+      import("vue-matomo").then((module) => {
+        const VueMatomo = module.default;
+        app.use(VueMatomo, {
+          host: "https://analytics.curiousers.org/",
+          siteId: 3,
+          router: router,
+          enableLinkTracking: true,
+          trackInitialView: true,
+          disableCookies: false,
+          requireConsent: false,
+          enableHeartBeatTimer: false,
+        });
+      });
+    }
   },
 });
